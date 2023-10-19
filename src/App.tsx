62ee15f0c30597350173d3111e40c58e1338c1ea
Mamanov Emil <emil.mamanov97@gmail.com>
2023-10-19 17:20:43 +0600

    finished project (for now)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import LotteryNumbers from './LotteryNumbers';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [currentNumbers, setCurrentNumbers] = useState([5, 11, 16, 23, 32]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Hi, it's my first expirience
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const generateNewNumbers = () => {
+        const newNumbers: number[] = [];
+        while (newNumbers.length < 5) {
+            const randomNumber = Math.floor(Math.random() * 32) + 5;
+            if (!newNumbers.includes(randomNumber)) {
+                newNumbers.push(randomNumber);
+            }
+        }
+        newNumbers.sort((a, b) => a - b); // Сортировка по возрастанию
+        setCurrentNumbers(newNumbers);
+    };
+
+    return (
+        <div className="App">
+            <h1>Lottery Numbers</h1>
+            <LotteryNumbers numbers={currentNumbers} />
+            <button onClick={generateNewNumbers}>New Numbers</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
