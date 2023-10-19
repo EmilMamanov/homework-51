f92acd9229e168ec5bd1fa4c9a651dfa9384667b
Mamanov Emil <emil.mamanov97@gmail.com>
2023-10-19 16:40:28 +0600

    added new .tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Hi, it's my first expirience
         </p>
       </div>
       <p className="read-the-docs">
